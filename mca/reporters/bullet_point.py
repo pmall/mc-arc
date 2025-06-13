380eaf7bea1bd0c379aed7adad16cf76566f5cc9
pmall <pierre.mallinjoud@gmail.com>
2025-06-13 17:56:28 +0200

    no need to be clever in the bullet point reporter

--- mca/reporters/bullet_point.py
+++ mca/reporters/bullet_point.py
@@ -2,12 +2,8 @@ from mca.interfaces import Message
 
 
 class BulletPointReporter:
-    def __call__(self, participant: str, messages: list[Message]) -> str:
-        replace = lambda p: "You" if p == participant else p
-
-        return "\n".join(
-            [f"- {replace(message.name)}: {message.content}" for message in messages]
-        )
+    def __call__(self, _: str, messages: list[Message]) -> str:
+        return "\n".join([f"- {message}" for message in messages])
 
 
 def create_bullet_point_reporter():
